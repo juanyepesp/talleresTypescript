50943c1c76f0993aea8c0c8cba75b4abbe1d6f24
juanyepesp <j.yepes@uniandes.edu.co>
2022-04-15 15:04:45 -0500

    entrega

--- main.ts
+++ main.ts
@@ -7,10 +7,7 @@ let seriesTbody: HTMLElement = document.getElementById('series')!;
 const totalSeasonsAvgElem: HTMLElement = document.getElementById("seasons-avg")!;
 let serieInfo: HTMLElement = document.getElementById('contentSerie')!;
 
-
-
 renderSeriesInTable(dataSeries);
-
 
 totalSeasonsAvgElem.innerHTML = `${getSeasonsAverage(dataSeries)}`
 
@@ -19,10 +16,29 @@ function renderSeriesInTable(series: Serie[]): void {
   console.log('Desplegando series');
   series.forEach((serie) => {
     let trElement = document.createElement("tr");
-    trElement.innerHTML = `<td>${serie.id}</td>
-                           <td><input type = "button" onclick = "applyFilterById(${serie.id})" value = "${serie.name}" id = serieID class="btn btn-link"></button></td>
-                           <td>${serie.channel}</td>
-                           <td>${serie.seasons}</td>`;
+
+    let serieIDElement = document.createElement("td");
+    serieIDElement.innerHTML = `${serie.id}`;
+
+    let serieNameElement = document.createElement("td");
+    serieNameElement.innerHTML = `<input type = "button" value = "${serie.name}" class="btn btn-link series-button"></button>`;
+      
+    let serieChannelElement = document.createElement("td");
+    serieChannelElement.innerHTML = `${serie.channel}`;
+
+    let serieSeasonsElement = document.createElement("td");
+    serieSeasonsElement.innerHTML = `${serie.seasons}`;
+
+    serieNameElement.addEventListener("click", function(){
+      console.log(`Rendering series with id = ${serie.id}`)
+      renderSerie(getSerieByID(serie.id+"", dataSeries));
+    })
+
+    trElement.appendChild(serieIDElement);
+    trElement.appendChild(serieNameElement);
+    trElement.appendChild(serieChannelElement);
+    trElement.appendChild(serieSeasonsElement);
+
     seriesTbody.appendChild(trElement);
   });
 }
@@ -33,34 +49,38 @@ function getSeasonsAverage(series: Serie[]): number {
   return (totalSeasons/series.length);
 }
 
-function getSerieByName(idKey: string, series: Serie[] ){
+function getSerieByID(idKey: string, series: Serie[] ){
   return idKey === '' ?  dataSeries : series.filter(se => se.id === parseInt(idKey));
 }
 
-
-function applyFilterById(id: string){
-  console.log(`Rendering series with id = ${id}`)
-  renderSerie(getSerieByName(id, dataSeries));
-}
-
-function renderSerie(series: Serie[]): void {
   
+function renderSerie(series: Serie[]): void {
+  let serieInfo: HTMLElement = document.getElementById('contentSerie')!;
+  
+  while (serieInfo.hasChildNodes()) {
+    if (serieInfo.firstChild != null) {
+      serieInfo.removeChild(serieInfo.firstChild);
+      
+    }
+  }
+
   series.forEach((serie) => {
-    console.log(`Mostrando serie de id = ${serie.id}`)
-
-    let titleElement = document.createElement("h1")
-    titleElement.append(`${serie.name}`);
-
-    let imgElement = document.createElement("div")
-    imgElement.innerHTML = `<img src = "${serie.imgLink}"><img style='height: 100%; width: 100%; object-fit: contain'/>`
-
-
-    let pElement = document.createElement("p")
-    pElement.append(`${serie.description}`)
-
-
-    serieInfo.append(titleElement);
-    serieInfo.append(imgElement);
-    serieInfo.append(pElement);
+      console.log(`Mostrando serie de id = ${serie.id}`)
+  
+      let titleElement = document.createElement("h1")
+      titleElement.append(`${serie.name}`);
+  
+      let imgElement = document.createElement("div")
+      imgElement.innerHTML = `<img src = "${serie.imgLink}" width="250">`
+  
+  
+      let pElement = document.createElement("p")
+      pElement.append(`${serie.description}`)
+  
+  
+      serieInfo.append(titleElement);
+      serieInfo.append(imgElement);
+      serieInfo.append(pElement);
+      serieInfo.innerHTML += `<a href="${serie.link}">${serie.link}</a>`;
   });
-}
+  }
--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,22 @@ function renderSeriesInTable(series) {
     console.log('Desplegando series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td><input type = \"button\" onclick = \"applyFilterById(").concat(serie.id, ")\" value = \"").concat(serie.name, "\" id = serieID class=\"btn btn-link\"></button></td>\n                           <td>").concat(serie.channel, "</td>\n                           <td>").concat(serie.seasons, "</td>");
+        var serieIDElement = document.createElement("td");
+        serieIDElement.innerHTML = "".concat(serie.id);
+        var serieNameElement = document.createElement("td");
+        serieNameElement.innerHTML = "<input type = \"button\" value = \"".concat(serie.name, "\" class=\"btn btn-link series-button\"></button>");
+        var serieChannelElement = document.createElement("td");
+        serieChannelElement.innerHTML = "".concat(serie.channel);
+        var serieSeasonsElement = document.createElement("td");
+        serieSeasonsElement.innerHTML = "".concat(serie.seasons);
+        serieNameElement.addEventListener("click", function () {
+            console.log("Rendering series with id = ".concat(serie.id));
+            renderSerie(getSerieByID(serie.id + "", dataSeries));
+        });
+        trElement.appendChild(serieIDElement);
+        trElement.appendChild(serieNameElement);
+        trElement.appendChild(serieChannelElement);
+        trElement.appendChild(serieSeasonsElement);
         seriesTbody.appendChild(trElement);
     });
 }
@@ -17,24 +32,27 @@ function getSeasonsAverage(series) {
     series.forEach(function (serie) { return totalSeasons = totalSeasons + serie.seasons; });
     return (totalSeasons / series.length);
 }
-function getSerieByName(idKey, series) {
+function getSerieByID(idKey, series) {
     return idKey === '' ? dataSeries : series.filter(function (se) { return se.id === parseInt(idKey); });
 }
-function applyFilterById(id) {
-    console.log("Rendering series with id = ".concat(id));
-    renderSerie(getSerieByName(id, dataSeries));
-}
 function renderSerie(series) {
+    var serieInfo = document.getElementById('contentSerie');
+    while (serieInfo.hasChildNodes()) {
+        if (serieInfo.firstChild != null) {
+            serieInfo.removeChild(serieInfo.firstChild);
+        }
+    }
     series.forEach(function (serie) {
         console.log("Mostrando serie de id = ".concat(serie.id));
         var titleElement = document.createElement("h1");
         titleElement.append("".concat(serie.name));
         var imgElement = document.createElement("div");
-        imgElement.innerHTML = "<img src = \"".concat(serie.imgLink, "\"><img style='height: 100%; width: 100%; object-fit: contain'/>");
+        imgElement.innerHTML = "<img src = \"".concat(serie.imgLink, "\" width=\"250\">");
         var pElement = document.createElement("p");
         pElement.append("".concat(serie.description));
         serieInfo.append(titleElement);
         serieInfo.append(imgElement);
         serieInfo.append(pElement);
+        serieInfo.innerHTML += "<a href=\"".concat(serie.link, "\">").concat(serie.link, "</a>");
     });
 }
